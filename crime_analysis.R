9508a70244360e4d457794a4ca9ed3981d777a76
Frankie Tam <frankie@Frankies-MBP.fios-router.home>
2018-11-06 01:35:47 -0500

    Initial check in

---
+++ crime_analysis.R
@@ -0,0 +1,36 @@
+## Messy Data and Machine Learning Homework 4
+## Paul Sergent, Ruoyu Zhu, Frankie Tam
+## Crime_Library.R
+
+source('crime_library.R')
+
+
+## Retrieve data from urls
+crime.data.full <- foreach(i=1:nrow(url.data), .combine='rbind') %dopar% {
+  
+  # Retrieve html content
+  content <- content_to_parsed_html(get_site_content(url.data$url[i]))
+  # Retrieve crime type
+  crime.type <- extract_all_crime_type(content)
+  # Retrieve crime hour
+  crime.date <- extract_all_crime_date(content)
+  # Create crime neighborhood
+  crime.neighborhood <- rep_len(url.data$neighborhood[i], length(crime.type))
+  
+  crime.data.temp <- cbind(crime.type, crime.date, crime.neighborhood)
+  
+  crime.data.temp
+}
+
+crime.data <- crime.data.full
+# Convert to tibble
+crime.data <- as.tibble(crime.data)
+
+# Convert to date string to POSIXct and extract hour from date
+crime.data <- crime.data %>% mutate(crime.date=mdy_hm(crime.date)) %>% mutate(hour=hour(crime.date))
+
+# Remove crime.date column
+crime.data <- select(crime.data, -crime.date)
+
+# Rename columns
+names(crime.data) <- c("crime", "neighborhood", "hour")
---
+++ crime_library.R
@@ -0,0 +1,107 @@
+## Messy Data and Machine Learning Homework 4
+## Paul Sergent, Ruoyu Zhu, Frankie Tam
+## Crime_Library.R
+
+library(httr)
+library(xml2)
+library(rvest)
+library(stringr)
+library(tidyverse)
+library(foreach)
+library(lubridate)
+
+## URLs of crime pages and neigborhood
+# urls <- read.table("urls.txt", header = FALSE)
+# names(urls) <- c("url");
+
+url <- c("https://www.universalhub.com/crime/allston.html",
+          "https://www.universalhub.com/crime/back-bay.html",
+          "https://www.universalhub.com/crime/beacon-hill.html",
+          "https://www.universalhub.com/crime/brighton.html",
+          "https://www.universalhub.com/crime/charlestown.html",
+          "https://www.universalhub.com/crime/dorchester.html",
+          "https://www.universalhub.com/crime/dorchester.html?page=1",
+          "https://www.universalhub.com/crime/downtown.html",
+          "https://www.universalhub.com/crime/east-boston.html",
+          "https://www.universalhub.com/crime/fenway.html",
+          "https://www.universalhub.com/crime/hyde-park.html",
+          "https://www.universalhub.com/crime/jamaica-plain.html",
+          "https://www.universalhub.com/crime/mattapan.html",
+          "https://www.universalhub.com/crime/mission-hill",
+          "https://www.universalhub.com/crime/north-end.html",
+          "https://www.universalhub.com/crime/roslindale.html",
+          "https://www.universalhub.com/crime/roxbury.html",
+          "https://www.universalhub.com/crime/south-boston.html",
+          "https://www.universalhub.com/crime/south-end.html",
+          "https://www.universalhub.com/crime/west-roxbury.html"
+          )
+
+neighborhood <- c("Allston",
+                  "Back Bay",
+                  "Beacon Hill",
+                  "Brighton",
+                  "Charlestown",
+                  "Dorchester",
+                  "Dorchester",
+                  "Downtown",
+                  "East Boston",
+                  "Fenway",
+                  "Hyde Park",
+                  "Jamaica Plain",
+                  "Mattapan",
+                  "Mission Hill",
+                  "North End",
+                  "Roslindale",
+                  "Roxbury",
+                  "South Boston",
+                  "South End",
+                  "West Roxbury")
+
+url.data <- tibble(url, neighborhood)
+
+## custom words
+custom_words <- read.table("custom_words.txt", header = FALSE)
+
+## Extract from UHubScript.R
+
+## FUNCTIONS TO GET THE XML PARSED HTML FROM SITE
+get_site_content <- function( url ){
+  require( httr )
+  # get the site response
+  response <- httr::GET( url )
+  # extract the content
+  content <- httr::content( x = response, as = 'text', encoding = 'utf-8' )
+  # return 
+  return( content )
+}
+
+content_to_parsed_html <- function( content ){
+  require( xml2 )
+  # parse the html with xml2
+  parsed_html <- xml2::read_html( content )
+  # return
+  return( parsed_html )
+}
+
+## Extract crime type
+extract_all_crime_type <- function( parsed_html ){
+  require( rvest )
+  # get all table cells of the appropriate class 
+  crime_type_els <- rvest::html_nodes( x = parsed_html, xpath = '//td[contains(@class,"field-name")]' )
+  # extract the text from the cell
+  crime_types <- rvest::html_text( crime_type_els )
+  # return
+  return( crime_types )
+}
+
+## Extract crime date
+extract_all_crime_date <- function( parsed_html ){
+  require( rvest )
+  # get all table cells of the appropriate class 
+  crime_date_els <- rvest::html_nodes( x = parsed_html, xpath = '//td[contains(@class,"crime-date")]' )
+  # extract the text from the cell
+  crime_dates <- rvest::html_text( crime_date_els )
+  
+  # return
+  return( crime_dates )
+}
